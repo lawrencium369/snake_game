e485d30fc1b79ff1dcf83df3620e604ea6dfd099
lawrence <lawrencemusonda555@gmail.com>
2025-06-11 23:01:14 +0100

    "minor code optimisation"

--- scoreboard.py
+++ scoreboard.py
@@ -15,20 +15,17 @@ class ScoreBoard(Turtle):
     def update_scoreboard(self):
         self.clear()
         self.write(arg=f"score:{self.score}  high score:{self.high_score()}", align="center", font=FONT)
-        
+
     def high_score(self):
         with open("data.txt") as file:
             new_highscore = file.read()
             return new_highscore
              
-    def update_highscore(self, text):
-        with open("data.txt", "w") as file:
-            file.write(text)
-
     def reset_scoreboard(self):
         try:
             if self.score > int(self.high_score()):
-              self.update_highscore(str(self.score))
+                with open("data.txt", "w") as file:
+                    file.write(str(self.score))
         except ValueError:
             return None
         self.score = 0
